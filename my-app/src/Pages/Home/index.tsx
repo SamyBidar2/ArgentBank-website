f08337b9547b335e822362dc7f0c8e02ffbc5014
SamyBidar2 <samybidarpro@gmail.com>
2024-06-24 11:44:19 +0200

    Création du composant Feature pour la page Home

--- my-app/src/Pages/Home/index.tsx
+++ my-app/src/Pages/Home/index.tsx
@@ -1,8 +1,9 @@
+import { FeatureItem } from '../../Components/FeatureItem';
 import './index.css';
-import Banner from '../../../../designs/img/bank-tree.jpeg';
-import Chat from '../../../../designs/img/icon-chat.png';
-import Money from '../../../../designs/img/icon-money.png';
-import Security from '../../../../designs/img/icon-security.png';
+import Banner from '../../../../designs/img/bank-tree.webp';
+import Chat from '../../../../designs/img/icon-chat.webp';
+import Money from '../../../../designs/img/icon-money.webp';
+import Security from '../../../../designs/img/icon-security.webp';
 
 export const Home = () => {
     return (
@@ -18,37 +19,9 @@ export const Home = () => {
         </div>
         <section className="features">
           <h2 className="sr-only">Features</h2>
-          <div className="feature-item">
-            <img src={Chat} alt="Chat Icon" className="feature-icon" />
-            <h3 className="feature-item-title">You are our #1 priority</h3>
-            <p>
-              Need to talk to a representative? You can get in touch through our
-              24/7 chat or through a phone call in less than 5 minutes.
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-              src={Money}
-              alt="Chat Icon"
-              className="feature-icon"
-            />
-            <h3 className="feature-item-title">More savings means higher rates</h3>
-            <p>
-              The more you save with us, the higher your interest rate will be!
-            </p>
-          </div>
-          <div className="feature-item">
-            <img
-              src={Security}
-              alt="Chat Icon"
-              className="feature-icon"
-            />
-            <h3 className="feature-item-title">Security you can trust</h3>
-            <p>
-              We use top of the line encryption to make sure your data and money
-              is always safe.
-            </p>
-          </div>
+            <FeatureItem logo= {Chat}/>
+            <FeatureItem logo= {Money}/>
+            <FeatureItem logo= {Security}/>
         </section>
       </main>
     )
